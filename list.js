0db64bfb2c5bf89e2867a4c8b5b6d45e1fdc4353
Sam Newman <newmansam@outlook.com>
2015-12-21 03:06:30 +0000

    added use strict

--- list.js
+++ list.js
@@ -1,25 +1,27 @@
-const React = require('react');
-const createElement = React.createElement;
-const cloneElement = React.cloneElement;
-const COMPONENT_LIST_TYPE = Symbol('@@ComponentList');
+'use strict';
+
+var React = require('react');
+var createElement = React.createElement;
+var cloneElement = React.cloneElement;
+var COMPONENT_LIST_TYPE = Symbol('@@ComponentList');
 
 function createClone(Comp, children, st, en) {
-  const newWrap = createListWrap(Comp);
+  var newWrap = createListWrap(Comp);
   
-  for (let i = st - 1; ++i <= en;)
+  for (var i = st - 1; ++i <= en;)
     newWrap.push(children[i]);
 
   return newWrap;
 }
 
 function createListWrap(Comp) {
-  let key = -1;
-  const components = [];
-  const ComponentList = props => createElement(Comp, props, components);
+  var key = -1;
+  var components = [];
+  var ComponentList = props => createElement(Comp, props, components);
   ComponentList.type = COMPONENT_LIST_TYPE;
 
   function iterateAdd(args, cb) {
-    for (let i = -1; ++i < args.length;)
+    for (var i = -1; ++i < args.length;)
       cb(cloneElement(args[i], { key: ++key }));
     return args.length;
   }
@@ -48,8 +50,8 @@ function createListWrap(Comp) {
 
   function append(r, n) {
     if (n.type && n.type === COMPONENT_LIST_TYPE) {
-      const c = n.slice();
-      for (let i = -1; ++i < c.length;)
+      var c = n.slice();
+      for (var i = -1; ++i < c.length;)
         r.push(c);
     } else {
       r.push(n);
@@ -57,11 +59,11 @@ function createListWrap(Comp) {
   }
 
   ComponentList.concat = function concat_() {
-    const clone = createClone(Comp, components, 0, components.length - 1);
+    var clone = createClone(Comp, components, 0, components.length - 1);
 
-    for (let i = -1; ++i < arguments.length;) {
+    for (var i = -1; ++i < arguments.length;) {
       if (Array.isArray(arguments[i])) {
-        for (let j = -1; ++j < arguments[i].length;)
+        for (var j = -1; ++j < arguments[i].length;)
           clone.push(arguments[i][j]);
       } else {
         append(clone, arguments[i]);
